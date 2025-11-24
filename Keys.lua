d9337d0e6519b2d36db419d309c5d3047f7130e8
jituconfiglite-cmyk <jituconfiglite@gmail.com>
2025-11-24 12:31:02 +0530

    Create Keys.lua

---
+++ Keys.lua
@@ -0,0 +1,4 @@
+return {
+  { key="JC-TEST-0001", bind="", days=7, active=true, reseller="main", expirydate="31-12-2025" },
+  { key="JC-TEST-0002", bind="", days=30, active=true, reseller="main", expirydate="31-12-2025" },
+}
